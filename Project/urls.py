9595f9490985e979aa1c81ea2f906d6d9fc10201
Akshay Kaushik <akshay.kaushik9@gmail.com>
2023-07-20 11:19:09 +0530

    Added static file support to all apps

--- Project/urls.py
+++ Project/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +26,11 @@ urlpatterns = [
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', include('core.urls')),
 ]
+
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.TE_URL,
+                          document_root=settings.STATIC_ROOT)
