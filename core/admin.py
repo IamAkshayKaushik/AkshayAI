eff55b9ad86e4ab9f6619bfa0be50d9985b07b1d
Akshay Kaushik <akshay.kaushik9@gmail.com>
2023-08-03 09:02:06 +0530

    Added Database Tables to Admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from core.models import User, AudioFile, TokenPurchase, UserAction, UserStripeRecord
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(AudioFile)
+admin.site.register(TokenPurchase)
+admin.site.register(UserAction)
+admin.site.register(UserStripeRecord)
